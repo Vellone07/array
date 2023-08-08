08671ae6c4b17ba71539416638486153fa457ec6
Vellone07 <davibittancourt@gmail.com>
2023-08-08 07:58:08 -0300

    edição do css

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-// FUNÇÃO PARA ARMAZENAR NOMES EM ARRAY
 var dadosLista = [];
 function salvarUser(){
-    let nomeUser=document.getElementById("nomeUser").value;
-    if(nomeUser){
-        dadosLista.push(nomeUser);
-        criarLista();
-    }
-}
 
+  let TarefaUser=document.getElementById("nomeUser").value;
+
+  if(TarefaUser){
+
+      dadosLista.push(TarefaUser);
+
+      criarLista();
+
+      document.getElementById('nomeUser').value = '';
+  }
+}
 // FUNÇÃO PARA CRIAR LISTA DE USUÁRIO
+
 function criarLista(){
-let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+
+let tabela = document.getElementById('tabela').innerHTML = "<tr><th> Tarefas </th><th>Ações</th></tr>";
+
 for(let i = 0;i <= (dadosLista.length - 1);i++){
 
-    tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger'  onclick=''>Excluir</button></td></tr>";
+  tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger'  onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
 
-    document.getElementById('tabela').innerHTML = tabela;
+  document.getElementById('tabela').innerHTML = tabela;
+
+  }
+}
+
+// FUNÇÃO PARA EDITAR NOME
+function editar(i){
+  document.getElementById('nomeUser').value = dadosLista [(i - 1)];
+  dadosLista.splice(dadosLista[(i-1)], 1);
 
 
+}
 
+// FUNÇÃO PARA EXCLUIR NOME
 
-    }
+function excluir(i){
+
+  dadosLista.splice((i - 1), 1);
+
+  document.getElementById('tabela').deleteRow(i);
+
 
 }
